0be2157b93eb6c629fda9f049c31fb6ded6cbcc3
Bozo Vuksan <bozovuksan@gmail.com>
2026-03-12 09:38:39 +0000

    Initial Project Setup

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import {Button, HStack} from "@chakra-ui/react"
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+  <HStack>
+    <Button>Click me</Button>
+    <Button>Click me</Button>
+  </HStack>
+  );
 }

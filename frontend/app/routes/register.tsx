35c8a52f70caf08f16fdfd7e1429cb3133d56451
jaspher <if24b110@technikum-wien.at>
2026-03-16 17:52:53 +0100

    Changed Register Component - added Input field component - changed register view

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -1,6 +1,13 @@
 import type { Route } from "./+types/register.tsx";
-import { Separator, Flex, Spacer, Heading, Field, Input, Link, Button, ButtonGroup, Box, Container, Center, AbsoluteCenter, FieldErrorText } from "@chakra-ui/react"
-import { useState } from "react";
+import { Separator, Flex, Spacer, Heading, Field, Input, Link, Button, ButtonGroup, Box, Container, Center, AbsoluteCenter} from "@chakra-ui/react"
+import { useState, type ChangeEvent } from "react";
+import { PasswordInput } from "~/components/ui/password-input.js";
+
+enum InputFieldState {
+    Untouched,
+    Valid,
+    Invalid,
+}
 
 interface RegisterProperties {
     username: string;
@@ -8,96 +15,99 @@ interface RegisterProperties {
 }
 
 
+
+// one single component - input field
+function InputField({
+    label,
+    placeholder = "email",
+    required = false,
+    confidential = false,
+    onValidate }:{
+        label: string,
+        placeholder?: string,
+        required?: boolean,
+        confidential?: boolean,
+        onValidate?:
+        (value: string) => {
+            isValid: boolean, 
+            errorMessage: string
+        }
+    }) {
+
+    // values/variable for the component
+    const [currentState, setCurrentState] = useState(InputFieldState.Untouched);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [value, setValue] = useState("Jaspher");
+
+    // logic inside component
+    function OnBlur() {
+        let state: InputFieldState = InputFieldState.Valid;
+
+        if (onValidate) {
+            let validationResult = onValidate(value);
+            if (!validationResult.isValid) {
+                state = InputFieldState.Invalid;
+                setErrorMessage(validationResult.errorMessage);
+            }
+        }
+
+        if (value == "") {
+            state = InputFieldState.Invalid;
+            setErrorMessage(`${label} is required!`);
+        }
+
+        setCurrentState(state);
+    }
+
+    // logic inside component
+    function OnChange(event: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+        setValue(event.target.value);
+    }
+
+    function GetInputComponent(confidential : boolean){
+        if(confidential){
+            return <PasswordInput placeholder={placeholder} onBlur={OnBlur} value={value} onChange={OnChange} ></PasswordInput>;
+        } else{
+            return <Input placeholder={placeholder} onBlur={OnBlur} value={value} onChange={OnChange} />;
+        }
+    }
+
+    // view of the component
+    return (
+        < Field.Root required={required} invalid={currentState == InputFieldState.Invalid && required}>
+            <Field.Label>
+                {label}
+                <Field.RequiredIndicator />
+            </Field.Label>
+            {GetInputComponent(confidential)}         
+            <Field.HelperText />
+            <Field.ErrorText >
+                {errorMessage}
+            </Field.ErrorText >
+        </ Field.Root >
+    )
+}
+
+// Register component 
 export default function Register({ loaderData }: Route.ComponentProps,) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
 
-    const [isUsernameEmpty, setIsUsernameEmpty] = useState(false);
-    const [isPasswordEmpty, setIsPasswordEmpty] = useState(false);
-
-    function onUsernameChanged(e: React.ChangeEvent<HTMLInputElement>) {
-        setUsername(e.target.value);
-    }
-
-    function onPasswordChanged(e: React.ChangeEvent<HTMLInputElement>) {
-        setPassword(e.target.value);
-    }
-
-    function CheckUsername(): string {
-        let errors: string[] = [];
-
-        let usernameRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
-
-        if (username === "") {
-            errors.push("Username is required");
-        }
-
-        if (!usernameRegex.test(username)) {
-            errors.push("Username must be a valid email address");
-        }
-
-        return errors.at(0) ?? "";
-    }
-
-    function onUsernameBlur() {
-        if (CheckUsername() != "") {
-            setIsUsernameEmpty(true);
-        } else {
-            setIsUsernameEmpty(false);
+    function onValidate(value: string) {
+        return {
+            isValid: value == 'a',
+            errorMessage: "Must be 'a'",
         }
     }
 
-    function CheckPassword() : string {
-        let errors: string[] = [];
-
-        let passwordRegex = /\S{8,}/;
-
-        if (password === "") {
-            errors.push("Password is required");
-        } 
-
-        console.log(password.match(passwordRegex));
-
-        if (password.match(passwordRegex) == null) {
-            errors.push("Password must be at least 8 characters long");
-        }
-
-        return errors.at(0) ?? "";
-    }
-
-        function onPasswordBlur() {
-        if (CheckPassword() != "") {
-            setIsPasswordEmpty(true);
-        } else {
-            setIsPasswordEmpty(false);
-        }
-    }
     return (
         <AbsoluteCenter>
             <Container maxW="md">
                 <Flex gap="5" direction="column">
                     <Heading>Register</Heading>
                     <Separator />
-                    <Field.Root invalid={isUsernameEmpty}>
-                        <Field.Label>
-                            Username | {username}
-                            <Field.RequiredIndicator />
-                        </Field.Label>
-                        <Input placeholder="tour@guide.com" value={username} onChange={onUsernameChanged} onBlur={onUsernameBlur} />
-                        <Field.HelperText />
-                        <Field.ErrorText>{CheckUsername()}</Field.ErrorText>
-                    </Field.Root>
-                    <Field.Root invalid={isPasswordEmpty}>
-                        <Field.Label>
-                            Password
-                            <Field.RequiredIndicator />
-                        </Field.Label>
-                        <Input placeholder="password" value={password} onChange={onPasswordChanged} onBlur={onPasswordBlur}/>
-                        <Field.HelperText />
-                        <Field.ErrorText>{CheckPassword()}</Field.ErrorText>
-                    </Field.Root>
+                    <InputField label="E-Mail" required onValidate={onValidate}></InputField>
+                    <InputField label="Password" required confidential></InputField>
                     <Link href="">Go to Login here</Link>
-                    <Button disabled={isUsernameEmpty || isPasswordEmpty} onClick={() => console.log("Register clicked")}>Register</Button>
+                    <Button onClick={() => console.log("Register clicked")}>Register</Button>
                 </Flex>
             </Container>
         </AbsoluteCenter>

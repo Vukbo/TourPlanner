39a42d39c803ca65c2d628d1d9b32dabb3613d20
jaspher <if24b110@technikum-wien.at>
2026-03-23 13:41:40 +0100

    Changed Register - For Query and Fetches

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -5,6 +5,8 @@ import { PasswordInput } from "~/components/ui/password-input.js";
 import { FormStates, stateReducer } from "~/reducer/form.js";
 import { useReducer } from "react";
 import InputField, { InputState, type InputModel } from "~/components/input.js";
+import { useQuery } from "@tanstack/react-query";
+import { Login } from "~/queries/user.js";
 
 // TODO: move to separate file 
 
@@ -17,18 +19,28 @@ export default function Register({ loaderData }: Route.ComponentProps,) {
 
     const [form, formDispatcher] = useReducer(stateReducer, { state: FormStates.Untouched, inputs: [usernameInput,passwordInput] });
 
-    return (
-        <AbsoluteCenter>
-            <Container maxW="md">
-                <Flex gap="5" direction="column">
-                    <Heading>Register</Heading>
-                    <Separator />
-                    <InputField model={usernameInput} onChange={(m)=> {setUsernameInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
-                    <InputField model={passwordInput} onChange={(m)=> {setPasswordInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
-                    <Link href="">Go to Login here</Link>
-                    <Button onClick={() => console.log("Register clicked")} disabled={!(form.state==FormStates.Valid)}>Register</Button>
-                </Flex>
-            </Container>
-        </AbsoluteCenter>
-    );
+    const {isPending, error, data, isFetching} = useQuery({
+        queryKey: ['login'], queryFn: () => Login({id: "1", password: "AVeryLongPassword_2000"})
+    })
+
+    if(isPending) return 'loading...'
+
+    if(error) return 'An error has occurred: ' + error.message
+
+    // if(data) return data.id
+    
+    // return (
+    //     <AbsoluteCenter>
+    //         <Container maxW="md">
+    //             <Flex gap="5" direction="column">
+    //                 <Heading>Register</Heading>
+    //                 <Separator />
+    //                 <InputField model={usernameInput} onChange={(m)=> {setUsernameInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
+    //                 <InputField model={passwordInput} onChange={(m)=> {setPasswordInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
+    //                 <Link href="">Go to Login here</Link>
+    //                 <Button onClick={() => console.log("Register clicked")} disabled={!(form.state==FormStates.Valid)}>Register</Button>
+    //             </Flex>
+    //         </Container>
+    //     </AbsoluteCenter>
+    // );
 }

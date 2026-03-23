3bc741362e8ded2a68c9414e03cd7a8bd1b87384
Bozo Vuksan <bozovuksan@gmail.com>
2026-03-23 11:27:49 +0100

    Refactors Register View

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -2,117 +2,20 @@ import type { Route } from "./+types/register.tsx";
 import { Separator, Flex, Spacer, Heading, Field, Input, Link, Button, ButtonGroup, Box, Container, Center, AbsoluteCenter, type JsxElement } from "@chakra-ui/react"
 import { useState, type ChangeEvent } from "react";
 import { PasswordInput } from "~/components/ui/password-input.js";
-import { stateReducer } from "~/reducer/form.js";
+import { FormStates, stateReducer } from "~/reducer/form.js";
 import { useReducer } from "react";
-import type { InputField } from "~/reducer/form.js";
-import { InputFieldState } from "~/reducer/form.js";
+import InputField, { InputState, type InputModel } from "~/components/input.js";
 
 // TODO: move to separate file 
 
-interface inputFieldProperties {
-    label: string,
-    placeholder?: string,
-    required?: boolean,
-    state: InputFieldState,
-    confidential?: boolean,
-    onValidate?:
-    (value: string) => {
-        isValid: boolean,
-        errorMessage: string
-    },
-    onStateChange?: (ifs: InputField) => void
-}
 
-
-
-// one single component - input field
-function InputField({ label, placeholder = "email", required = false, state = InputFieldState.Untouched, confidential = false, onValidate, onStateChange }: inputFieldProperties) {
-
-    // values/variable for the component
-    const [errorMessage, setErrorMessage] = useState("");
-    const [value, setValue] = useState("Jaspher");
-
-    // logic inside component
-    function OnBlur() {
-        let state: InputFieldState = InputFieldState.Valid;
-
-        if (onValidate) {
-            let validationResult = onValidate(value);
-            if (!validationResult.isValid) {
-                state = InputFieldState.Invalid;
-                setErrorMessage(validationResult.errorMessage);
-            }
-        }
-
-        if (value == "") {
-            state = InputFieldState.Invalid;
-            setErrorMessage(`${label} is required!`);
-        }
-
-        if (onStateChange)
-            onStateChange({ label: label, required: required, state: state }) ;
-    }
-
-    // logic inside component
-    function OnChange(event: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
-        setValue(event.target.value);
-    }
-
-    function GetInputComponent(confidential: boolean) {
-        if (confidential) {
-            return <PasswordInput placeholder={placeholder} onBlur={OnBlur} value={value} onChange={OnChange} ></PasswordInput>;
-        } else {
-            return <Input placeholder={placeholder} onBlur={OnBlur} value={value} onChange={OnChange} />;
-        }
-    }
-
-    // view of the component
-    return (
-        < Field.Root required={required} invalid={state == InputFieldState.Invalid && required}>
-            <Field.Label>
-                {label} / {state}
-                <Field.RequiredIndicator />
-            </Field.Label>
-            {GetInputComponent(confidential)}
-            <Field.HelperText />
-            <Field.ErrorText >
-                {errorMessage}
-            </Field.ErrorText >
-        </ Field.Root >
-    )
-}
 
 // Register component 
 export default function Register({ loaderData }: Route.ComponentProps,) {
-    const [unif, setUnif] = useState({ label: "E-Mail", required: false,  state: InputFieldState.Untouched }) ;
-    const [pwif, setPwif] = useState({ label: "Password", required: true, state: InputFieldState.Untouched}) ;
+    const [usernameInput, setUsernameInput] = useState<InputModel>({id:"Username",placeholder:"mail@domain.com",required:true,state:InputState.Untouched,value:""}) ;
+    const [passwordInput, setPasswordInput] = useState<InputModel>({id:"Password",required:true,confidential:true,placeholder:"********",state:InputState.Untouched,value:""}) ;
 
-    const [state, dispatch] = useReducer(stateReducer, { canSubmit: false, inputs: [unif, pwif] });
-    // function in01(l:string,r:boolean) {
-    //     return (
-    //                 <InputField label={l} required={r} onValidate={onValidate} onStateChange={onStageChange}></InputField>
-    //     );
-    // }
-
-
-    function onStageChangeUnif(ifs: InputField) {
-        dispatch({ type: "changeInputState", value: ifs })
-        //return state.inputs.find((e) => e.label == ifs.label)!.state;
-        setUnif({...unif, state: ifs.state});
-    }
-
-    function onStageChangePwif(ifs: InputField) {
-        dispatch({ type: "changeInputState", value: ifs })
-        //return state.inputs.find((e) => e.label == ifs.label)!.state;
-        setPwif({...pwif, state: ifs.state});
-    }
-
-    function onValidate(value: string) {
-        return {
-            isValid: value == 'a',
-            errorMessage: "Must be 'a'",
-        }
-    }
+    const [form, formDispatcher] = useReducer(stateReducer, { state: FormStates.Untouched, inputs: [usernameInput,passwordInput] });
 
     return (
         <AbsoluteCenter>
@@ -120,10 +23,10 @@ export default function Register({ loaderData }: Route.ComponentProps,) {
                 <Flex gap="5" direction="column">
                     <Heading>Register</Heading>
                     <Separator />
-                    <InputField label={unif.label} required={unif.required} state={unif.state} onValidate={onValidate} onStateChange={onStageChangeUnif}></InputField>
-                    <InputField label={pwif.label} required={pwif.required} state={pwif.state} onValidate={onValidate} onStateChange={onStageChangePwif} confidential></InputField>
+                    <InputField model={usernameInput} onChange={(m)=> {setUsernameInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
+                    <InputField model={passwordInput} onChange={(m)=> {setPasswordInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
                     <Link href="">Go to Login here</Link>
-                    <Button onClick={() => console.log("Register clicked")} disabled={!state.canSubmit}>Register</Button>
+                    <Button loading onClick={() => console.log("Register clicked")} disabled={!(form.state==FormStates.Valid)}>Register</Button>
                 </Flex>
             </Container>
         </AbsoluteCenter>

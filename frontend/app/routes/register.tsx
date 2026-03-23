2e5e4fb32c33db4e3330b5eea306438b3da12cff
jaspher <if24b110@technikum-wien.at>
2026-03-23 17:49:22 +0100

    Changes register - adds axios library

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -5,10 +5,14 @@ import { PasswordInput } from "~/components/ui/password-input.js";
 import { FormStates, stateReducer } from "~/reducer/form.js";
 import { useReducer } from "react";
 import InputField, { InputState, type InputModel } from "~/components/input.js";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { Login } from "~/queries/user.js";
+import type { RegisterModel, UserModel } from "~/models/user.js";
+import type { Register } from "react-router";
+import axios from "axios";
 
 // TODO: move to separate file 
+
 
 
 
@@ -16,31 +20,41 @@ import { Login } from "~/queries/user.js";
 export default function Register({ loaderData }: Route.ComponentProps,) {
     const [usernameInput, setUsernameInput] = useState<InputModel>({id:"Username",placeholder:"mail@domain.com",required:true,state:InputState.Untouched,value:""}) ;
     const [passwordInput, setPasswordInput] = useState<InputModel>({id:"Password",required:true,confidential:true,placeholder:"********",state:InputState.Untouched,value:""}) ;
+    const [user, setUser] = useState<RegisterModel>({username: "", password : ""})
 
-    const [form, formDispatcher] = useReducer(stateReducer, { state: FormStates.Untouched, inputs: [usernameInput,passwordInput] });
+    async function onSubmit(){
+        let u = {username : usernameInput.value, password : passwordInput.value};
+        setUser(u);
 
-    const {isPending, error, data, isFetching} = useQuery({
-        queryKey: ['login'], queryFn: () => Login({id: "1", password: "AVeryLongPassword_2000"})
-    })
+        // const mutation = useMutation({
+        //     mutationFn: (register : RegisterModel) => {
+        //         return axios.post("http://localhost:2406/auth/register", user)
+        //     }
+        // })
+        let response = await axios.post("http://localhost:2406/auth/register", u)
+        setUser({username : response.data.id, password : passwordInput.value})
+    }
 
-    if(isPending) return 'loading...'
+    const [form, formDispatcher] = useReducer(stateReducer, { state: FormStates.Untouched, inputs: [usernameInput,passwordInput]});
 
-    if(error) return 'An error has occurred: ' + error.message
+    // model
 
-    if(data) return data.id
+    // if(isPending) return 'loading...'
+
+    // if(error) return 'An error has occurred: ' + error.message
     
-    // return (
-    //     <AbsoluteCenter>
-    //         <Container maxW="md">
-    //             <Flex gap="5" direction="column">
-    //                 <Heading>Register</Heading>
-    //                 <Separator />
-    //                 <InputField model={usernameInput} onChange={(m)=> {setUsernameInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
-    //                 <InputField model={passwordInput} onChange={(m)=> {setPasswordInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
-    //                 <Link href="">Go to Login here</Link>
-    //                 <Button onClick={() => console.log("Register clicked")} disabled={!(form.state==FormStates.Valid)}>Register</Button>
-    //             </Flex>
-    //         </Container>
-    //     </AbsoluteCenter>
-    // );
+    return (
+        <AbsoluteCenter>
+            <Container maxW="md">
+                <Flex gap="5" direction="column">
+                    <Heading>Register {user.username}</Heading>
+                    <Separator />
+                    <InputField model={usernameInput} onChange={(m)=> {setUsernameInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
+                    <InputField model={passwordInput} onChange={(m)=> {setPasswordInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
+                    <Link href="">Go to Login here</Link>
+                    <Button onClick={() => formDispatcher({type: "submit", action: onSubmit})} disabled={!(form.state==FormStates.Valid)}>Register</Button>
+                </Flex>
+            </Container>
+        </AbsoluteCenter>
+    );
 }

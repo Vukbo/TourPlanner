fc7e343f2117c8820039f58af2831ded3df0f02d
jaspher <if24b110@technikum-wien.at>
2026-03-30 12:25:37 +0200

    Changes Register View - Adds Form Logic

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/register.tsx";
-import { Separator, Flex, Spacer, Heading, Field, Input, Link, Button, ButtonGroup, Box, Container, Center, AbsoluteCenter, type JsxElement } from "@chakra-ui/react"
+import { Separator, Flex, Spacer, Heading, Field, Input, Link, Button, ButtonGroup, Box, Container, Center, AbsoluteCenter, type JsxElement, type ConditionalValue } from "@chakra-ui/react"
 import { useState, type ChangeEvent } from "react";
 import { PasswordInput } from "~/components/ui/password-input.js";
 import { FormStates, stateReducer } from "~/reducer/form.js";
@@ -8,22 +8,52 @@ import InputField, { InputState, type InputModel } from "~/components/input.js";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { Login } from "~/queries/user.js";
 import type { RegisterModel, UserModel } from "~/models/user.js";
-import type { Register } from "react-router";
+import { Navigate, redirect, useNavigate, type Register } from "react-router";
 import axios from "axios";
+import { Alert } from "@chakra-ui/react"
+import { handler } from "~/queries/rest.js";
 
 // TODO: move to separate file 
 
+enum RegisterStates {
+    Success,
+    Error,
+    Pending,
+    Idle
+}
 
-
+interface RegisterState {
+    title: string,
+    description: string,
+    state: RegisterStates,
+}
 
 // Register component 
 export default function Register({ loaderData }: Route.ComponentProps,) {
-    const [usernameInput, setUsernameInput] = useState<InputModel>({id:"Username",placeholder:"mail@domain.com",required:true,state:InputState.Untouched,value:""}) ;
-    const [passwordInput, setPasswordInput] = useState<InputModel>({id:"Password",required:true,confidential:true,placeholder:"********",state:InputState.Untouched,value:""}) ;
-    const [user, setUser] = useState<RegisterModel>({username: "", password : ""})
+    const [usernameInput, setUsernameInput] = useState<InputModel>({ id: "Username", placeholder: "mail@domain.com", required: true, state: InputState.Untouched, value: "" });
+    const [passwordInput, setPasswordInput] = useState<InputModel>({ id: "Password", required: true, confidential: true, placeholder: "********", state: InputState.Untouched, value: "" });
+    const [user, setUser] = useState<RegisterModel>({ username: "", password: "" })
+    const [registerState, setRegisterState] = useState<RegisterState>({ title: "", description: "", state: RegisterStates.Idle })
+    let navigate = useNavigate();
 
-    async function onSubmit(){
-        let u = {username : usernameInput.value, password : passwordInput.value};
+    const mutation = useMutation({
+        mutationFn: async (user: RegisterModel) => {
+            try {
+                let response = await handler.post("/auth/register", user);
+                return response;
+            }
+            catch (error) {
+                throw error;
+            }
+        },
+        onSuccess : () => {
+            console.log("Hello!")
+            navigate("/");
+        }
+    })
+
+    async function onSubmit() {
+        let u = { username: usernameInput.value, password: passwordInput.value };
         setUser(u);
 
         // const mutation = useMutation({
@@ -31,28 +61,75 @@ export default function Register({ loaderData }: Route.ComponentProps,) {
         //         return axios.post("http://localhost:2406/auth/register", user)
         //     }
         // })
-        let response = await axios.post("http://localhost:2406/auth/register", u)
-        setUser({username : response.data.id, password : passwordInput.value})
+        // let response = await axios.post("http://localhost:2406/auth/register", u)
+        try {
+            setRegisterState({ title: "", description: "", state: RegisterStates.Pending });
+            let response = await mutation.mutateAsync(u);
+            setUser({ username: response.data.id, password: passwordInput.value })
+            setRegisterState({ title: "Registration successful!", description: "User registration was successful", state: RegisterStates.Success });
+        } catch (error) {
+            setRegisterState({ title: "Registration failed!", description: (error as Error).message, state: RegisterStates.Error });
+        }
     }
 
-    const [form, formDispatcher] = useReducer(stateReducer, { state: FormStates.Untouched, inputs: [usernameInput,passwordInput]});
+    function ShowRegisterState(state: RegisterState) {
+
+        type statusType = ConditionalValue<"info" | "success" | "warning" | "error" | "neutral" | undefined>
+        let status: statusType = "info";
+
+        let title
+        let description
+
+        if (state.state === RegisterStates.Idle || state.state === RegisterStates.Pending) {
+            return;
+        }
+
+        if (state.state === RegisterStates.Success) {
+            status = "success"
+            title = state.title
+            description = state.description
+        }
+
+        if (state.state === RegisterStates.Error) {
+            status = "error"
+            title = state.title
+            description = state.description
+        }
+
+        return (
+            <Alert.Root status={status}>
+                <Alert.Indicator />
+                <Alert.Content>
+                    <Alert.Title >
+                        {title}
+                    </Alert.Title>
+                    <Alert.Description>
+                        {description}
+                    </Alert.Description>
+                </Alert.Content>
+            </Alert.Root>
+        )
+    }
+
+    const [form, formDispatcher] = useReducer(stateReducer, { state: FormStates.Untouched, inputs: [usernameInput, passwordInput] });
 
     // model
 
     // if(isPending) return 'loading...'
 
     // if(error) return 'An error has occurred: ' + error.message
-    
+
     return (
         <AbsoluteCenter>
             <Container maxW="md">
                 <Flex gap="5" direction="column">
                     <Heading>Register {user.username}</Heading>
                     <Separator />
-                    <InputField model={usernameInput} onChange={(m)=> {setUsernameInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
-                    <InputField model={passwordInput} onChange={(m)=> {setPasswordInput(m); formDispatcher({type: "changeInputState", input:m})}}></InputField>
+                    {ShowRegisterState(registerState)}
+                    <InputField model={usernameInput} onChange={(m) => { setUsernameInput(m); formDispatcher({ type: "changeInputState", input: m }) }}></InputField>
+                    <InputField model={passwordInput} onChange={(m) => { setPasswordInput(m); formDispatcher({ type: "changeInputState", input: m }) }}></InputField>
                     <Link href="">Go to Login here</Link>
-                    <Button onClick={() => formDispatcher({type: "submit", action: onSubmit})} disabled={!(form.state==FormStates.Valid)}>Register</Button>
+                    <Button loading={registerState.state == RegisterStates.Pending ? true : false} onClick={() => formDispatcher({ type: "submit", action: onSubmit })} disabled={!(form.state == FormStates.Valid)}>Register</Button>
                 </Flex>
             </Container>
         </AbsoluteCenter>

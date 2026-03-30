0d89870f9c46b6ad3f8e4fdbb6b5b2943b1d725e
jaspher <if24b110@technikum-wien.at>
2026-03-30 13:36:30 +0200

    Adds Map view

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import {Button, HStack} from "@chakra-ui/react"
+import Map from "./map";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,8 +14,7 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
   <HStack>
-    <Button>Click me</Button>
-    <Button>Click me</Button>
+    <Map/>
   </HStack>
   );
 }

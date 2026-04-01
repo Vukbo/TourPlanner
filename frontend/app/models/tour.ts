aef15915f955371e776115c5220724a880e1fcaa
vukbo <bozovuksan@gmail.com>
2026-04-01 15:15:46 +0200

    Adds Tour Model

---
+++ frontend/app/models/tour.ts
@@ -0,0 +1,16 @@
+export enum TransportTypes {
+    Hiking=0,
+    Biking=1
+}
+
+export interface Tour {
+    id?: string,
+    author: string,
+    title : string,
+    description? : string,
+    from : string,
+    to : string,
+    transportType: TransportTypes,
+    distance?: number,
+    duration?: number
+}

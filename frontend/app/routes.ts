857f31bcbfad0ad71bfc4103da2385cec6921983
vukbo <bozovuksan@gmail.com>
2026-04-01 18:53:21 +0200

    adds map route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("views/login.tsx"),
+    route("map", "views/map.tsx"),
     route("register", "views/register.tsx"),
     route("tours", "views/tours.tsx"),
     route("tour-management", "views/tour-management.tsx"),

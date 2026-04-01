9ec6c4a519ad495d816fc32c828ef6939bef88f3
vukbo <bozovuksan@gmail.com>
2026-04-01 18:53:13 +0200

    Adds missing leaflet dependencies

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -35,6 +35,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossOrigin=""/>
+      <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossOrigin=""></script>
       </head>
       <body>
         {children}

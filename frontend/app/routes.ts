deeb4f7f993349a42ae78288261c69e359279a68
vukbo <bozovuksan@gmail.com>
2026-04-01 15:17:35 +0200

    Adds tour-management route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -4,5 +4,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [index("routes/home.tsx"),
     route("register", "routes/register.tsx"),
     route("tours", "routes/tours.tsx"),
+    route("tour-management", "routes/tour-management.tsx"),
 ] satisfies RouteConfig;
 
